f28b3662e2a140856c904eed87bc62443d5715ac
田风有 <420144542@qq.com>
2022-05-14 15:35:33 +0800

    Tina阿基
    Tina阿基

---
+++ TFYSwiftCategoryUtil/TFYSwiftCategoryUtil/Category/String+Chain.swift
@@ -0,0 +1,48 @@
+//
+//  String+Chain.swift
+//  TFYSwiftCategoryUtil
+//
+//  Created by 田风有 on 2022/5/13.
+//
+
+import Foundation
+import UIKit
+import CommonCrypto
+
+extension String {
+    
+    var md5 : String{
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = CC_LONG(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+        CC_SHA256(str!, strLen, result)
+        let hash = NSMutableString()
+        for i in 0 ..< digestLen {
+          hash.appendFormat("%02x", result[i])
+        }
+        result.deinitialize(count: digestLen)
+        return String(format: hash as String)
+      }
+    
+    
+    func widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: height), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.width)
+    }
+    
+    func heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.height)
+    }
+                                                                                                                                                
+    func heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.height)>maxHeight ? maxHeight : ceil(rect.height)
+    }
+   
+}
+
